0423c5be1d2856e402687aa790ec05f1d61f1d0a
Marcia1134 <mossmarcia1134@gmail.com>
2024-03-30 10:10:46 +0800

    Fixed bug with Age Range Selection Menu

--- src/assets/introduction/question2_point_5_pnts_birth.py
+++ src/assets/introduction/question2_point_5_pnts_birth.py
@@ -32,6 +32,10 @@ class birth_view(View):
         self.add_item(self.dropdown)
 
     async def interaction_check(self, interaction: Interaction):
-        Introduction.create(user_id=interaction.user.id, part=1, Introduction=f"age range = {self.dropdown.values[0]}")
+        data = self.dropdown.values
+        logger.debug(f"Dropdown: Birth | User: {interaction.user} | Data: {data}")
+        Introduction.create(user_id=interaction.user.id, part=1, introduction=f"age range = {self.dropdown.values[0]}")
         logger.debug("Added user's birth year range to the database.")
         await interaction.response.edit_message(embed=question3_ask_reason.reason_embed, view=question3_ask_reason.reason_view())
+
+    
